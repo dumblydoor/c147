1eacd2f8321b54e6920d69199d8eea6fc3ec3599
dumblydoor <96972278+dumblydoor@users.noreply.github.com>
2023-07-10 09:21:17 +1000

    Add files via upload

---
+++ project 147.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 09:18:44 2023
+
+@author: aquilavijayanayagam
+"""
+
+from tkinter import*
+import random
+root=Tk()
+root.title("dictionary")
+root.geometry("400x400")
+
+
+
+root = Tk()
+root.title("Encryption with Ascii Code")
+
+root.geometry("400x400")
+root.configure(background = 'lightblue1')
+
+enter_word = Entry(root)
+enter_word.place(relx = 0.5, rely = 0.4, anchor = CENTER)
+
+label = Label(root, text = "Name in Ascii : ", bg = "light cyan", fg = "black")
+label2 = Label(root, text = "Encrypted Name : ", bg = "light cyan", fg = "black")
+
+def asciiConverter():
+    input_word = str(enter_word.get())
+    
+    for letter in input_word:
+        label["text"] += str(ord(letter)) + " "
+        ascii = int(ord(letter))
+        encrypted = ascii - 1
+        label2["text"] += str(chr(encrypted))
+        
+btn = Button(root, text = "Display the Ascii Code & Encrypted value", command = asciiConverter, bg = 'cornflower blue', fg = 'white')
+btn.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+label.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+label2.place(relx = 0.5, rely = 0.7, anchor = CENTER)
+
+root.mainloop() 
+from tkinter import *
+
+root = Tk()
+root.title("Encryption with Ascii Code")
+
+root.geometry("400x400")
+root.configure(background = 'lightblue1')
+
+enter_word = Entry(root)
+enter_word.place(relx = 0.5, rely = 0.4, anchor = CENTER)
+
+label = Label(root, text = "Name in Ascii : ", bg = "light cyan", fg = "black")
+label2 = Label(root, text = "Encrypted Name : ", bg = "light cyan", fg = "black")
+
+def asciiConverter():
+    input_word = str(enter_word.get())
+    
+    for letter in input_word:
+        label["text"] += str(ord(letter)) + " "
+        ascii = int(ord(letter))
+        encrypted = ascii - 1
+        label2["text"] += str(chr(encrypted))
+        
+btn = Button(root, text = "Display the Ascii Code & Encrypted value", command = asciiConverter, bg = 'cornflower blue', fg = 'white')
+btn.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+label.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+label2.place(relx = 0.5, rely = 0.7, anchor = CENTER)
